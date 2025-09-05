3c3960af51a55f1476fe76f54903599911991a37
Patima Poochai <patimapoochai@gmail.com>
2025-09-04 20:18:31 -1000

    Change website title and base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "13 Days of Observability",
+    pageTitle: "Up to Speed on Observability",
     pageTitleSuffix: "by Patima Poochai",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "observability.patimapoochai.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
