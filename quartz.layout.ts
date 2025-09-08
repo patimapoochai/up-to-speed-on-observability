654cc826f32175369c3ddd4f5b5154730ba703f1
Patima Poochai <patimapoochai@gmail.com>
2025-09-07 15:28:00 -1000

    Remove quartz discord link from footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,8 +19,7 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/patimapoochai/up-to-speed-on-observability",
     },
   }),
 }
